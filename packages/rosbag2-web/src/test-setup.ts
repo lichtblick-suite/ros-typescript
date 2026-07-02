b85d8fdfb91f722b1740002cf79780f3987a2c7a
Gabriela Almeida <gabriela.almeida@criticaltechworks.com>
2026-07-02 13:58:40 +0100

    fix(ci): fix TS2578/TS2345 in rosbag and TS5112 in rosmsg-msgs-common

--- packages/rosbag2-web/src/test-setup.ts
+++ packages/rosbag2-web/src/test-setup.ts
@@ -1,6 +1,5 @@
 import { TextEncoder, TextDecoder } from "node:util";
 
 // JSDOM does not provide TextEncoder and TextDecoder
-// @ts-expect-error ignore type mismatch with util TextEncoder and global one
 globalThis.TextEncoder = TextEncoder;
 globalThis.TextDecoder = TextDecoder as typeof globalThis.TextDecoder;
